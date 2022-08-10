4fc4b40af5a41181720a74dbe409d816f14dee9a
connorGA <ronnoccap@gmail.com>
2022-08-09 22:20:57 -0700

    finished assessment

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let newNum = 0;
+
+const answer = document.querySelector('h2');
+const addButton = document.querySelector('#button1');
+const subtractButton = document.querySelector('#button2');
+const input = document.querySelector('#text');
+
+
+//Subtraction
+subtractButton.addEventListener("click", () => {
+    newNum -= parseInt(input.value);
+    answer.textContent = newNum;
+    if (newNum < 0){
+        answer.style.color = 'red';
+    } else {
+        answer.style.color = 'black';
+    }
+});
+
+//Addition
+addButton.addEventListener("click", () =>{
+    newNum += parseInt(input.value);
+    answer.textContent = newNum;
+    if (newNum < 0){
+        answer.style.color = 'red';
+    } else {
+        answer.style.color = 'black';
+    }
+});
